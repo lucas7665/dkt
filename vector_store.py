08c43311f3acb992305efa12e89470ed5954afbe
Lenovo <chonglucas@163.com>
2024-12-18 10:19:45 +0800

    init

--- vector_store.py
+++ vector_store.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 import os
 
-# 抑制警告1
+# 抑制警告12
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", message="Using.*TRANSFORMERS_CACHE.*")
 
